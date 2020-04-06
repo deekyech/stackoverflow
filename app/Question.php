1037817c4fb405237aa06d8e81c03a2a88b87bc9
Dhiresh <dhireshk.hirani@gmail.com>
2020-04-06 12:28:50 +0530

    Seeders and factories made for questions and users

--- app/Question.php
+++ app/Question.php
@@ -2,7 +2,8 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\BaseModel;
+use App\BaseModel;
+use Illuminate\Support\Str;
 
 class Question extends BaseModel
 {
@@ -15,4 +16,22 @@ class Question extends BaseModel
 	/*public function user() {
 		return $this->belongsTo(User::class);
 	}*/
+	
+	
+	/*
+	 * This following concept is called mutator.
+	 * When laravel maps each table to a class, it doesn't create actual variables in model class.
+	 * It inserts each attribute in the attributes array. And by using php magic methods, it creates getters and setters for all attributes.
+	 * So the format for these setter is setAttributeNameAttribute()
+	 * Eg: setTitleAttribute(), setVotesCountAttribute()
+	 * Whenever a programmer tries to set the title externally too, laravel internally calls this method to set the title.
+	 * This method sets the attribute value in the attributes array.
+	 *
+	 * Hence, by the following method, whenever a title is set while seeding, a slug will be generated at the same time.
+	 */
+	public function setTitleAttribute($title) {
+		$title = rtrim($title, ".");
+		$this->attributes['title'] = $title;
+		$this->attributes['slug'] = Str::slug($title);
+	}
 }
---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Question;
+use Faker\Generator as Faker;
+
+$factory->define(Question::class, function (Faker $faker) {
+    return [
+        'title'=> $faker->sentence(rand(5, 10)),
+	    'body'=> $faker->paragraph(rand(3, 7), true),
+	    'views_count'=> rand(0, 10),
+	    'answers_count'=> rand(0, 10),
+	    'votes_count'=> rand(-10, 10)
+    ];
+});
